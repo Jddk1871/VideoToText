1d0fc33c54832b86dd08d1c1afe21d54318ba3a7
Christopher Walther <christopher.walther@rohde-schwarz.com>
2023-03-21 08:30:18 +0100

    Video Capture __init__

--- Video Processing.py
+++ Video Processing.py
@@ -6,46 +6,57 @@ from time import sleep
 
 
 class VideoCapture:
+    #__ = privat
+    #_ = geschützt
+    def __init__(self, path):
+        self.__path = path
+        self.__vidFrames = []
 
-    def VideoToImages(path, newVideoWidth=800):
-        stream = cv2.VideoCapture(path)
-        current_frame = 0
+    def VideoToImages(self, newVideoWidth=800):
+        #newVideoWidth in pixeln
+        stream = cv2.VideoCapture(self.__path)
+        currentFrame = 0
         max_frames = stream.get(7)
 
         print("-----Video Info-----")
-        print("Breite: ", stream.get(3))
-        print("Höhe: ", stream.get(4))
+        print("Width: ", stream.get(3))
+        print("Height: ", stream.get(4))
         print("FPS: ", stream.get(5))
         print("Frames: ", max_frames)
         print("--------------------")
 
-        dimX_alt = newVideoWidth
-        dimY_alt = (stream.get(4) / stream.get(3)) * dimX_alt
-        print(f"x: {dimX_alt}, y: {dimY_alt}")
-        dim = (int(dimX_alt), int(dimY_alt))
-
-        vid_frames = []
+        dimXalt = newVideoWidth
+        dimYalt = (stream.get(4) / stream.get(3)) * dimXalt
+        print(f"New Dimensions:\nx: {dimXalt}, y: {dimYalt}")
+        dim = (int(dimXalt), int(dimYalt))
 
         # Video abfangen und in einzelne frames aufteilen
         print("Reading Video file")
         while (stream.isOpened()):
-            print(end="\r" f"Current Frame: {current_frame} von {max_frames}")
-            current_frame += 1
+            print(end="\r" f"Current Frame: {currentFrame} von {max_frames}")
+            currentFrame += 1
             ret, frame = stream.read()
 
             # Wenn das Video zu ende ist , hört das ganze auf
             if frame is not None:
                 resized = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
-                grey_frame = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
-                cv2.imwrite(f"dump/{current_frame}.jpg", grey_frame)
-
-
+                greyFrame = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+                #Speichert alle frames
+                #cv2.imwrite(f"dump/{current_frame}.jpg", grey_frame)
+                self.__vidFrames.append(greyFrame)
 
             else:
                 stream.release()
+                print("\nFinished reading video file")
                 break
 
 
+
 if __name__ == '__main__':
-    VideoCapture.VideoToImages('media/testMovie.mp4')
+    start = VideoCapture('media/testMovie.mp4')
+    start.VideoToImages()
+
+
+
+
 
