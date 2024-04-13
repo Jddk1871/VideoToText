9868903d474227b2be5b4591ab2e201606bbf4e1
Jddk1871 <christopherwalther@outlook.com>
2024-04-13 19:02:27 +0200

    Additional args

--- README.md
+++ README.md
@@ -1,19 +1,24 @@
 # Video-Ascii-Art-Converter
 
 Dieses Python-Skript liest ein Video ein und wandelt jedes Frame in ASCII um, bevor es auf der Konsole ausgegeben wird. 
-<br/> Es bietet die Möglichkeit, die Framerate des Ausgabevideos anzupassen.
+<br/> Es bietet die Möglichkeit, die Frame-rate des Ausgabevideos anzupassen.
 
 ## Install 
-1. Python Version 3.12, sollte aber auch mit niedrigern gehen >3.8. 
+1. Python Version 3.12, sollte aber auch mit niedrigen gehen >3.8. 
 2. Requirements.txt installieren
 
 ## Verwendung 
-Führen Sie das Skript aus und geben Sie den Pfad zur Videodatei an, die Sie verarbeiten möchten, sowie die Ziel-Framerate (optional).
+Führen Sie das Skript aus und geben Sie den Pfad zur Videodatei an, die Sie verarbeiten möchten, sowie die Ziel-Frame-rate (optional).
 ````
 py ConvertVideoToText.py --file video.mp4 --fps
 ````
 
 ## Optionen
 - `--file`: Pfad zur Videodatei, die verarbeitet werden soll. (Erforderlich)
-- `--fps`: Ziel-Framerate des Ausgabevideos. Muss niedriger sein als die tatsächliche Framerate des Eingabevideos. (Optional)
+- `--fps`: Ziel-frame-rate des Ausgabevideos. Muss niedriger sein als die tatsächliche Frame-rate des Eingabevideos. (Optional)
 
+
+
+# ToDo
+- Cuda Implementierung -> Jeden Frame aufspalten für schnellere berechnung 
+- Automatisches skalieren der Konsole, auf eine kleinere Zeichengröße 
--- V2/ConvertVideoToText.py
+++ V2/ConvertVideoToText.py
@@ -5,10 +5,16 @@ import numpy as np
 import cv2
 
 
-def convert_video_frames(path: str, target_fps: int | None, width: int = 400, chunk_size: int = 4):
+def convert_video_frames(path: str, target_fps: int | None, char_line_width: int | None, chunk_size: int | None):
     video_capture = cv2.VideoCapture(path)
     original_fps: float = video_capture.get(cv2.CAP_PROP_FPS)
+
     target_fps = target_fps if target_fps is not None else original_fps
+    char_line_width = char_line_width if char_line_width is not None else 100
+    chunk_size = chunk_size if chunk_size is not None else 4
+
+    width: int = char_line_width * chunk_size
+
     print("Target fps:", target_fps)
     skip_frames: int = int(original_fps / target_fps)
 
@@ -36,7 +42,7 @@ def convert_video_frames(path: str, target_fps: int | None, width: int = 400, ch
     video_capture.release()
 
 
-def convert_frame_to_ascii(image, dim: (int, int), chunk_size: int = 4) -> list[str]:
+def convert_frame_to_ascii(image, dim: (int, int), chunk_size: int) -> list[str]:
     frame: list[str] = []
     for y in range(0, dim[0], chunk_size*2):
         line = ""
@@ -79,13 +85,19 @@ def display_frame(ascii_frame: list[str], start_time: float, fps: int):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Program to process a video.")
+    parser = argparse.ArgumentParser(description="Program to convert a video to ascii art.")
     parser.add_argument("--file", dest="file", help="Video file", required=True)
     parser.add_argument("--fps", dest="fps", help="target fps, must be lower than actual", required=False)
+    parser.add_argument("--width", dest="width", help="target width in char len. "
+                                                      "80: 1 line is 80 characters long", required=False)
+    parser.add_argument("--chunk_size", dest="chunk_size", help="Chunk size", required=False)
+
     args = parser.parse_args()
 
     main_path: str = args.file
-    main_fps = int(args.fps) if args.fps is not None else None
+    main_fps: int | None = int(args.fps) if args.fps is not None else None
+    main_width: int | None = int(args.width) if args.width is not None else None
+    main_chunk_size: int | None = int(args.chunk_size) if args.chunk_size is not None else None
 
     # global vars
     skipped_frames: int = 0
@@ -93,4 +105,4 @@ if __name__ == "__main__":
     if not os.path.exists(main_path):
         print("Invalid file name")
     else:
-        convert_video_frames(os.path.join(os.curdir, main_path), main_fps)
+        convert_video_frames(os.path.join(os.curdir, main_path), main_fps, main_width, main_chunk_size)
